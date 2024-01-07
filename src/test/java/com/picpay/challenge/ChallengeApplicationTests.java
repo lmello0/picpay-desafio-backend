b588adcd033fe8300871343af1abe6e11f72402c
lmello <melloluc02@gmail.com>
2024-01-07 01:08:03 -0300

    :tada: init: Initial commit

---
+++ src/test/java/com/picpay/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
